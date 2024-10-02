b20614758a279a9df73c31c3bcfa9e70f50d90b4
Haneul Jang <haneul_jang@eva.mpg.de>
2024-10-02 10:15:59 +0200

    Update household-decision-making-model.R including age effects

--- household-decision-making-model.R
+++ household-decision-making-model.R
@@ -26,9 +26,7 @@ library(gganimate)
 library(posterior)
 library(distributional)
 
-##########################################################
-#######          Read cleaned data                 #######
-##########################################################
+####### Read data #######
 
 d = read.csv("hh_decision_data.csv", header=T, sep=",")
 
@@ -49,12 +47,12 @@ d_eth$score[d_eth$response == "Partner"] = 0
 
 table(d_eth$score, useNA="always")
 
-###############################################################
-####    Model 1:  Women autonomy score ~ ethnicity*domain  ####
-###############################################################
+######################
+####    Model 1   ####
+######################
 d_eth$score=ordered(as.factor(d_eth$score))
 
-get_prior(score ~ Ethnicity*domain2 + (1 | ind_id), data=d_eth, family=cumulative())
+get_prior(score ~ Ethnicity*domain2 + age + (1 | ind_id), data=d_eth, family=cumulative())
 
 m0 <- brm(score ~ (1 | ind_id),
           data=d_eth, 
@@ -64,22 +62,23 @@ m0 <- brm(score ~ (1 | ind_id),
             set_prior("student_t(3, 0, 2.5)", class = "sd")), 
           iter = 2000, warmup = 1000, cores = 1, chains = 4)
 
-m1 <- brm(score ~ Ethnicity*domain2 +
-            (1 | ind_id),
-          data=d_eth, 
-          family=cumulative("logit"),
-          prior = c(
-            set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
-            set_prior("student_t(3, 0, 2.5)", class = "sd")),
-          iter = 2000, warmup = 1000, cores = 1, chains = 4)
+m1 <- brm(score ~ Ethnicity*domain2 + age + 
+              (1 | ind_id),
+            data=d_eth, 
+            family=cumulative("logit"),
+            prior = c(
+              set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
+              set_prior("student_t(3, 0, 2.5)", class = "sd")),
+            iter = 2000, warmup = 1000, cores = 1, chains = 4)
 
 summary(m1)
+conditional_effects(m1)
 conditional_effects(m1, effects = "Ethnicity:domain2")
-
+conditional_effects(m1, effects = "age")
 
 ###### Model comparison ######
 m0 <- add_criterion(m0, criterion = "loo")
-m1 <- add_criterion(m1, criterion = "loo") 
+m1 <- add_criterion(m1.1, criterion = "loo") 
 
 loo::loo_compare(loo(m0), loo(m1))
 
@@ -104,6 +103,29 @@ pp_check(m1, type = "dens_overlay_grouped", group = "ind_id")
 # ECDF plots for each focal woman 
 pp_check(m1, type = "ecdf_overlay_grouped", group = "ind_id") 
 
+##### check random effects
+ranef_var <- VarCorr(m1)
+print(ranef_var)
+
+# Extract posterior samples for residual standard deviation (sigma)
+posterior <- posterior_samples(m1)
+
+# Check if sigma exists in the posterior
+print(names(posterior))  # Look for a parameter called "sigma" or similar
+
+# Extract sigma (residual SD) from the posterior
+residual_sd <- posterior$sd_ind_id__Intercept  # Assuming "sigma" is the name of the residual SD
+
+# Compute residual variance by squaring sigma
+residual_var <- mean(residual_sd)^2  # Taking the mean across posterior samples for sigma
+
+# Extract the standard deviation for focal_woman_ID (random effect)
+focal_woman_sd <- ranef_var$ind_id$sd[1]  # First element corresponds to the SD
+focal_woman_var <- focal_woman_sd^2  # Variance for focal_woman_ID
+
+# Compute ICC
+icc <- focal_woman_var / (focal_woman_var + residual_var)
+print(icc)
 
 ################################################################################
 #######                      BaYaka WOMEN versus MEN                     #######
@@ -127,12 +149,12 @@ d_sex$score[d_sex$response == "Partner" & d_sex$Sex == "Male"] = 2
 
 table(d_sex$score, useNA="always")
 
-#########################################################
-####    Model 2:  Women autonomy score ~ sex*domain  ####
-#########################################################
+###################
+####  Model 2  ####
+###################
 d_sex$score=ordered(as.factor(d_sex$score))
 
-get_prior(score ~ Sex*domain2 + (1 | ind_id), data=d_sex, family=cumulative())
+get_prior(score ~ Sex*domain2 + age + (1 | ind_id), data=d_sex, family=cumulative())
 
 m0_sex <- brm(score ~ (1 | ind_id),
               data=d_sex, 
@@ -142,19 +164,7 @@ m0_sex <- brm(score ~ (1 | ind_id),
                 set_prior("student_t(3, 0, 2.5)", class = "sd")), 
               iter = 2000, warmup = 1000, cores = 1, chains = 4)
 
-m2 <- brm(score ~ Sex*domain2 +
-            (1 | ind_id),
-          data=d_sex, 
-          family=cumulative("logit"),
-          prior = c(
-            set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
-            set_prior("student_t(3, 0, 2.5)", class = "sd")),
-          iter = 2000, warmup = 1000, cores = 1, chains = 4)
-
-summary(m2)
-conditional_effects(m2)
-
-m2.1 <- brm(score ~ Sex + domain2 +
+m2 <- brm(score ~ Sex*domain2 + age + 
               (1 | ind_id),
             data=d_sex, 
             family=cumulative("logit"),
@@ -163,8 +173,22 @@ m2.1 <- brm(score ~ Sex + domain2 +
               set_prior("student_t(3, 0, 2.5)", class = "sd")),
             iter = 2000, warmup = 1000, cores = 1, chains = 4)
 
+summary(m2)
+
+m2.1 <- brm(score ~ Sex + domain2 + age +
+                (1 | ind_id),
+              data=d_sex, 
+              family=cumulative("logit"),
+              prior = c(
+                set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
+                set_prior("student_t(3, 0, 2.5)", class = "sd")),
+              iter = 2000, warmup = 1000, cores = 1, chains = 4)
+
 summary(m2.1)
 conditional_effects(m2.1)
+conditional_effects(m2.1, effects = "Sex")
+conditional_effects(m2.1, effects = "domain2")
+conditional_effects(m2.1, effects = "age")
 
 ###### Model comparison ######
 m0_sex <- add_criterion(m0_sex, criterion = "loo")
@@ -189,7 +213,10 @@ pp_check(m2.1, type = "dens_overlay")
 
 # We can also check the model predictions by group witin the data
 pp_check(m2, type = "dens_overlay_grouped", group = "Sex") 
-pp_check(m2.1, type = "dens_overlay_grouped", group = "domain2") 
+pp_check(m2.1, type = "dens_overlay_grouped", group = "domain") 
+
+pp_check(m2, type = "dens_overlay_grouped", group = "Sex") 
+pp_check(m2.1, type = "dens_overlay_grouped", group = "domain") 
 
 # check for focal Id
 pp_check(m2, type = "dens_overlay_grouped", group = "ind_id") 
@@ -199,4 +226,4 @@ pp_check(m2.1, type = "dens_overlay_grouped", group = "ind_id")
 pp_check(m2, type = "ecdf_overlay_grouped", group = "ind_id") 
 pp_check(m2.1, type = "ecdf_overlay_grouped", group = "ind_id") 
 
-
+#save.image(file = "hh_decision_making.RData") 
